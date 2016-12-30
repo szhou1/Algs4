c6cdf2d13b2e1f9d28ecf6815f4b043d8f57e1a6
szhou1 <zhousteve3@gmail.com>
2016-12-30 19:11:57 +0700

    Format Percolation

--- src/Percolation.java
+++ src/Percolation.java
@@ -5,15 +5,16 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     private int N;
-    private int [] matrix;
+    private int[] matrix;
     private int openCount;
     private WeightedQuickUnionUF uf;
-    
+
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
-        if(n <= 0) throw new IllegalArgumentException();
+        if (n <= 0)
+            throw new IllegalArgumentException();
         N = n;
-        matrix = new int [n * n];
+        matrix = new int[n * n];
         openCount = 0;
         uf = new WeightedQuickUnionUF(n * n + 2);
     }
@@ -21,31 +22,31 @@ public class Percolation {
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
-        int r = row - 1; 
+        int r = row - 1;
         int c = col - 1;
-        if(matrix[r * N + c]==0) {
-            if(row == 1) {
+        if (matrix[r * N + c] == 0) {
+            if (row == 1) {
                 uf.union(N * N, r * N + c);
-            } else if(row == N) {
+            } else if (row == N) {
                 uf.union(N * N + 1, r * N + c);
-            } 
+            }
             checkSides(r, c, r, c - 1); // left
             checkSides(r, c, r - 1, c); // top
             checkSides(r, c, r, c + 1); // right
             checkSides(r, c, r + 1, c); // bottom
             matrix[r * N + c] = 1;
-            openCount++;                
+            openCount++;
         }
     }
-    
+
     private void checkSides(int r, int c, int i, int j) {
-        if(i >= 0 && i < N && j >= 0 && j < N && matrix[i * N + j]==1) {
+        if (i >= 0 && i < N && j >= 0 && j < N && matrix[i * N + j] == 1) {
             uf.union(r * N + c, i * N + j);
         }
     }
-    
+
     private void validate(int r, int c) {
-        if(r < 1 || r > N || c < 1 || c > N) {
+        if (r < 1 || r > N || c < 1 || c > N) {
             throw new IndexOutOfBoundsException();
         }
     }
@@ -53,15 +54,17 @@ public class Percolation {
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
-        row--; col--;
+        row--;
+        col--;
         return matrix[row * N + col] == 1;
     }
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
-        row--; col--;
-        return matrix[row * N + col]==1 && uf.connected(row * N + col, N * N);
+        row--;
+        col--;
+        return matrix[row * N + col] == 1 && uf.connected(row * N + col, N * N);
     }
 
     // number of open sites
@@ -73,18 +76,18 @@ public class Percolation {
     public boolean percolates() {
         return uf.connected(N * N, N * N + 1);
     }
-    
+
     private void printMatrix() {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < matrix.length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             sb.append(matrix[i] + " ");
-            if(i % N == N-1) {                
+            if (i % N == N - 1) {
                 sb.append("\n");
             }
         }
         StdOut.println(sb.toString());
     }
-    
+
     public static void main(String[] args) {
         int n = StdIn.readInt();
         Percolation perc = new Percolation(n);
@@ -93,7 +96,7 @@ public class Percolation {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
             perc.open(p, q);
-            if(perc.percolates()) {
+            if (perc.percolates()) {
                 break;
             }
         }
