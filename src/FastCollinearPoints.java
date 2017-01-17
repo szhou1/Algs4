7ee8dc57464019289a9285ad071bf0732d4f0cb5
szhou1 <zhousteve3@gmail.com>
2017-01-17 15:19:40 -0800

    Complete FastCollinearPoints

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -14,20 +14,54 @@ public class FastCollinearPoints {
     
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
-        Arrays.sort(points, points[0].slopeOrder());
-        
+        Arrays.sort(points);
+//        printArray(points);
         lineSegments = new ArrayList<LineSegment>();
         
-        for(int p = 0; p < points.length; p++) {
+        for(int i = 0; i < points.length; i++) {
+            Point origin = points[i];
             
-//            List<Double> slopes = new ArrayList<Double>();
+            Point [] temp = Arrays.copyOf(points, points.length);
             
-            for(int q = p+1; q < points.length; q++) {
-                double slope = points[p].slopeTo(points[q]);
+            Arrays.sort(temp, origin.slopeOrder());
+//            System.out.println("************************* Sorted slope order for " + origin);
+//            printArray(temp);
+            
+            int counter = 1;
+            Point min = origin;
+            Point max = origin;
+            double slope = origin.slopeTo(temp[1]);
+            
+            for(int q = 2; q < points.length; q++) {
+                double slope2 = origin.slopeTo(temp[q]);
+//                System.out.println(slope2 + " " + temp[q]);
+                if(slope == slope2) {
+//                    System.out.println("Found same adj slope: " + slope + " with " + temp[q]);
+                    counter++;
+//                    System.out.println("min: " + min);
+                    max = max.compareTo(temp[q]) < 0 ? temp[q] : max;
+                    max = max.compareTo(temp[q-1]) < 0 ? temp[q-1] : max;
+                    min = min.compareTo(temp[q]) > 0 ? temp[q] : min;
+                    min = min.compareTo(temp[q-1]) > 0 ? temp[q-1] : min;
+//                    System.out.println("min after: " + min);
+
+                } else {
+                    if(counter >= 3 && min == origin) {
+//                        System.out.println("Counter size: " + counter);
+//                        System.out.println(origin + " -> " + max);
+                        lineSegments.add(new LineSegment(origin, max));
+                    }
+                    counter = 1;
+                    max = origin;
+                    min = origin;
+                }
+                slope = slope2;
                 
             }
             
-            
+            if(counter >= 3 && min == origin) {
+                lineSegments.add(new LineSegment(origin, max));
+            }
             
         }
     }
@@ -43,6 +77,12 @@ public class FastCollinearPoints {
         return lineSegments.toArray(arr);
     }
     
+    private void printArray(Object[] array) {
+        for(Object o : array) {
+            System.out.println(o);
+        }
+    }
+    
     public static void main(String[] args) {
         int k = StdIn.readInt();
 
@@ -54,23 +94,23 @@ public class FastCollinearPoints {
         }
         
         // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
+//        StdDraw.enableDoubleBuffering();
+//        StdDraw.setXscale(0, 32768);
+//        StdDraw.setYscale(0, 32768);
+//        for (Point p : points) {
+//            p.draw();
+//        }
+//        StdDraw.show();
 
         FastCollinearPoints brute = new FastCollinearPoints(points);
         System.out.println("Total segments: " + brute.numberOfSegments());
         for (LineSegment ls : brute.segments()) {
             if(ls != null) {                
                 System.out.println(ls);
-                ls.draw();
+//                ls.draw();
             }
         }
-        StdDraw.show();
+//        StdDraw.show();
     }
 
 }
